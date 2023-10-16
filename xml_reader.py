2cfbcb4769fda8bd1ed07592e1d8248ef6c625b8
FabioVillar <fabio_santos1710@outlook.com>
2023-10-16 20:52:34 -0300

    removed menu

--- xml_reader.py
+++ xml_reader.py
@@ -2,50 +2,6 @@ import json
 
 from bs4 import BeautifulSoup
 
-def printingMenu():
-    global allDirectivesArray
-    print("_________________________________________________________________")
-    print("Welcome to the menu\nPrinting all cpp:directives found...")
-    i = 1
-    for directive in allDirectivesArray:
-        print(i, directive)
-        i += 1
-        
-def printingMenu2():
-    global allDirectivesArray, directiveIndex
-    print("_________________________________________________________________")
-    directiveIndex = int(input("Please choose which directive you want more details: "))
-    while directiveIndex < 0 or directiveIndex > len(allDirectivesArray):
-        directiveIndex = int(input("\nPlease choose which directive you want more details: "))
-    directiveChoosen = allDirectivesArray[directiveIndex-1]
-    print("_________________________________________________________________")
-    print("\nPrinting more information about ", directiveChoosen)
-
-def menu():
-    global bs_data, directiveIndex
-    while True:
-        printingMenu()
-        directiveChoosen = []
-        directiveIndex = -1
-        printingMenu2()
-        count = 0
-        for data in bs_data.find_all():
-            if data.name == 'directive':
-                count += 1
-                if count == directiveIndex:
-                    if data.next == 'if' or data.next == 'else' or data.next == 'elif':
-                        index = bs_data.find_all().index(data)
-                        while bs_data.find_all()[index].name != 'endif':
-                            print(bs_data.find_all()[index].text)
-                            index+=1
-                        print(bs_data.find_all()[index].text)
-                    elif data.next == 'include' or data.next == 'define':
-                        index = bs_data.find_all().index(data)
-                        print(bs_data.find_all()[index].text)
-                        print(bs_data.find_all()[index+1].text)
-                        print(bs_data.find_all()[index+2].text)
-                    break
-
 def getElseConditional(i):
     conditional = ""
     while bs_data.find_all()[i].next != 'if':
@@ -114,11 +70,7 @@ def main():
     for data in bs_data.find_all():
         if data.name == 'directive':
             allDirectivesArray.append(data)
-    #vou ignorar o menu
-    #menu()
     findConditionals()
-    #print("Printing all conditionals found in xml...\n\n")
-    #print(allConditionalsDic)
     json_name = 'conditionals_' + c_file_name[:-6] + '.json'
     with open(json_name, 'w', newline='') as json_file:
         json.dump(allConditionalsDic, json_file)
@@ -130,5 +82,3 @@ bs_data = None
 directiveIndex = None
 c_file_name = 'test3.c.xml'
 main()
-
-#ifdefs deram erro
